9c388d2f39dada55386b432d4530ab7226919f98
Miski Ali <miskiali222@gmail.com>
2022-06-17 18:20:54 -0500

    added div, Id's, and got the submit button to work

--- client.js
+++ client.js
@@ -2,4 +2,26 @@ $(onReady);
 
 function onReady(){
     console.log('works')
+//submit button click listener
+$('#submitButton').on('click', addsValue);
+
 }
+
+
+
+function addsValue(){
+    let firstNameEver = $('.firstName').val();
+    let lastNameGreatest = $('.lastName').val();
+    let employeeIdInfo = $('.employeeId').val();
+    let jobTitleInfo = $('.jobTitle').val();
+    let annualsalaryInfo = $('.annualSalary').val();
+
+// let info = $('#Fri').val()
+    $('#first-name').append(firstNameEver);
+    $('#last-name').append(lastNameGreatest);
+    $('#employee').append(employeeIdInfo);
+    $('#job-title').append(jobTitleInfo);
+    $('#annual-salary').append(annualsalaryInfo);
+
+} 
+// end of function that adds value to the table
